04c74c86bea4fa9002eb15c03cbfdad99478c767
KurnosovNikita <Kurnosov-nikita@mail.ru>
2020-01-05 17:09:31 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function toReadable (number) {
-  
-}
+const NUMBERS = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+const DOZENS = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+module.exports = toReadable = n => {
+    if(n===0) {
+        return 'zero';
+    }
+    n = `00${n}`.substr(-3).match(/^(\d{1})(\d{2})$/);
+    return `${(n[1] != 0) ? (NUMBERS[Number(n[1])] || DOZENS[n[1][0]] + ' ' + NUMBERS[n[1][1]]) + 'hundred ' : ''}${((n[2] != 0) ? (NUMBERS[Number(n[2])] || DOZENS[n[2][0]] + ' ' + NUMBERS[n[2][1]]) : '')}`.trim();
+};
